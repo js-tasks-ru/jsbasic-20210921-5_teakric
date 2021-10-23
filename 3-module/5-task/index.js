bdde5abbee93467e006c2a9c5350ae04ec16fee4
KaterinaTap <teakric@gmail.com>
2021-10-24 02:01:01 +0300

    module-3

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,10 @@
 function getMinMax(str) {
   // ваш код...
-}
+  let arr =  str.split(' ')
+           .filter( item => + item)
+           .sort( (a, b) => a - b );
+    return result = {
+      min : + arr.shift(),
+      max : + arr.pop()
+    };
+} 
