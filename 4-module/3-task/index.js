e8ea23178b9376956f47cdc903240b6d435c3754
KaterinaTap <teakric@gmail.com>
2021-10-25 18:21:40 +0300

    module-4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
   // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    if (row.cells[3].hasAttribute(`data-available`)) {
+
+      if (row.cells[3].getAttribute(`data-available`) =='true') {
+        row.classList.toggle('available');
+      }
+      if (row.cells[3].getAttribute(`data-available`) =='false') {
+        row.classList.toggle('unavailable');
+      }
+    } else {
+      row.setAttribute('hidden', true);
+    }
+    if (row.cells[2].textContent =='m') {
+      row.classList.toggle('male');
+    }
+    if (row.cells[2].textContent =='f') {
+      row.classList.toggle('female');
+    }
+    if (row.cells[1].textContent < 18) {
+      row.style="text-decoration: line-through";
+    }
+  } 
 }
