23cc5005c3ed376cf20acf7965b0f23ec266c05e
KaterinaTap <teakric@gmail.com>
2021-10-28 18:20:50 +0300

    module-5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,16 @@
 function toggleText() {
   // ваш код...
+  const btn = document.querySelector('.toggle-text-button');
+  const text = document.querySelector('#text');
+  text.hidden = false;
+
+  btn.addEventListener('click', onBtnClick);
+
+  function onBtnClick() {
+    if ( text.hidden == true ) {
+      text.hidden = false; 
+    } else {
+      text.hidden = true;
+    } 
+  }
 }
